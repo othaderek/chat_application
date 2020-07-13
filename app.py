3780783075294ab448b1775f68ebe99e54264705
othaderek <otha.hernandez@gmail.com>
2020-07-13 16:36:54 -0400

    incomplete

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-import os
 import pdb
 
 # Init App
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+# Init SQLAlchemy
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    return {"msg": "hello"}
 
 # Databse Config
-app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/chat_app_api'
+db.
 
 pdb.set_trace()
