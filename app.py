e080b9774c84b46593b2cf0ebce6227bdafbabf3
othaderek <otha.hernandez@gmail.com>
2020-07-13 17:07:49 -0400

    /users route returns all users in postgres db as JSON.

--- app.py
+++ app.py
@@ -24,3 +24,14 @@ class UsersModel(db.Model):
     def __repr__(self):
         return f"<User {self.name}>"
 
+# get users
+@app.route('/users', methods=["GET"])
+def get_users():
+    if request.method == 'GET':
+        users = UsersModel.query.all()
+        results = [
+            {
+                'name': user.name
+            }for user in users]
+    return jsonify(results)
+        
